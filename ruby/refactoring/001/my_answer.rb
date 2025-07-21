65fd80e1dfced5aa3b44cdc2b4226a230793ef38
bongtzeyaw <bongtzeyaw@gmail.com>
2025-07-21 14:34:19 +0900

    値段の率の適用をDRYにする

--- ruby/refactoring/001/my_answer.rb
+++ ruby/refactoring/001/my_answer.rb
@@ -5,21 +5,15 @@ class ReservationSystem
 
     if room_type == 1
       price = 8000
-      price *= 1.5 if date[5..6] == '08'
-      price *= 0.9 if user[0] == 'G'
-      "Single room reserved for #{date}. Price: #{price}"
+      "Single room reserved for #{date}. Price: #{apply_rate(price, date, user)}"
 
     elsif room_type == 2
       price = 12_000
-      price *= 1.5 if date[5..6] == '08'
-      price *= 0.9 if user[0] == 'G'
-      "Double room reserved for #{date}. Price: #{price}"
+      "Double room reserved for #{date}. Price: #{apply_rate(price, date, user)}"
 
     elsif room_type == 3
       price = 20_000
-      price *= 1.5 if date[5..6] == '08'
-      price *= 0.9 if user[0] == 'G'
-      "Suite reserved for #{date}. Price: #{price}"
+      "Suite reserved for #{date}. Price: #{apply_rate(price, date, user)}"
 
     else
       'error'
@@ -30,5 +24,11 @@ class ReservationSystem
 
   def check_date(date)
     date[0..3].to_i >= 2024 && date[5..6].to_i >= 1 && date[5..6].to_i <= 12 && date[8..9].to_i >= 1 && date[8..9].to_i <= 31
+  end
+
+  def apply_rate(price, date, user)
+    price *= 1.5 if date[5..6] == '08'
+    price *= 0.9 if user[0] == 'G'
+    price
   end
 end
