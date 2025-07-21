355b0b71eba8393eb24a00b112b64ec96dbc998b
bongtzeyaw <bongtzeyaw@gmail.com>
2025-07-21 14:28:07 +0900

    日付のチェックをDRYにする

--- ruby/refactoring/001/my_answer.rb
+++ ruby/refactoring/001/my_answer.rb
@@ -1,32 +1,21 @@
 class ReservationSystem
   def check_reservation(user, room_type, date)
     return 'error' if user.nil?
+    return 'error' unless check_date(date)
 
     if room_type == 1
-      unless date[0..3].to_i >= 2024 && date[5..6].to_i >= 1 && date[5..6].to_i <= 12 && date[8..9].to_i >= 1 && date[8..9].to_i <= 31
-        return 'error'
-      end
-
       price = 8000
       price *= 1.5 if date[5..6] == '08'
       price *= 0.9 if user[0] == 'G'
       "Single room reserved for #{date}. Price: #{price}"
 
     elsif room_type == 2
-      unless date[0..3].to_i >= 2024 && date[5..6].to_i >= 1 && date[5..6].to_i <= 12 && date[8..9].to_i >= 1 && date[8..9].to_i <= 31
-        return 'error'
-      end
-
       price = 12_000
       price *= 1.5 if date[5..6] == '08'
       price *= 0.9 if user[0] == 'G'
       "Double room reserved for #{date}. Price: #{price}"
 
     elsif room_type == 3
-      unless date[0..3].to_i >= 2024 && date[5..6].to_i >= 1 && date[5..6].to_i <= 12 && date[8..9].to_i >= 1 && date[8..9].to_i <= 31
-        return 'error'
-      end
-
       price = 20_000
       price *= 1.5 if date[5..6] == '08'
       price *= 0.9 if user[0] == 'G'
@@ -35,5 +24,11 @@ class ReservationSystem
     else
       'error'
     end
+  end
+
+  private
+
+  def check_date(date)
+    date[0..3].to_i >= 2024 && date[5..6].to_i >= 1 && date[5..6].to_i <= 12 && date[8..9].to_i >= 1 && date[8..9].to_i <= 31
   end
 end
