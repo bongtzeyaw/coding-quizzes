a8fdd17a67647484b48f5da0c828af0f71aa6655
bongtzeyaw <bongtzeyaw@gmail.com>
2025-08-10 02:16:08 +0900

    レベルアップ関連メソッドと攻撃関連メソッドの並び順を調整し、シンプルなメソッドを先に置き、可読性を向上

--- ruby/refactoring/002/my_answer.rb
+++ ruby/refactoring/002/my_answer.rb
@@ -14,6 +14,15 @@ class GameCharacter
     @defense = defense
   end
 
+  def level_up
+    increments = level_up_increments
+
+    @hp += increments[:hp]
+    @mp += increments[:mp]
+    @attack += increments[:attack]
+    @defense += increments[:defense]
+  end
+
   def attack_enemy(enemy, skill)
     return 0 unless valid_attack_skill?(skill)
 
@@ -23,15 +32,6 @@ class GameCharacter
     consume_mp!(attack_skill_detail)
     damage = calculate_damage(attack_skill_detail)
     enemy.receive_damage!(damage)
-  end
-
-  def level_up
-    increments = level_up_increments
-
-    @hp += increments[:hp]
-    @mp += increments[:mp]
-    @attack += increments[:attack]
-    @defense += increments[:defense]
   end
 
   protected
@@ -44,12 +44,12 @@ class GameCharacter
 
   private
 
-  def attack_skill_details
-    ATTACK_SKILLS_DETAIL.merge(self.class::ATTACK_SKILLS_DETAIL)
-  end
-
   def level_up_increments
     self.class::LEVEL_UP_STATS_INCREMENTS
+  end
+
+  def attack_skill_details
+    ATTACK_SKILLS_DETAIL.merge(self.class::ATTACK_SKILLS_DETAIL)
   end
 
   def valid_attack_skill?(skill)
@@ -74,15 +74,15 @@ class GameCharacter
 end
 
 class Warrior < GameCharacter
-  ATTACK_SKILLS_DETAIL = {
-    'slash' => { mp_cost: 5, attack_multiplier: 1.5 }
-  }.freeze
-
   LEVEL_UP_STATS_INCREMENTS = {
     hp: 30,
     mp: 5,
     attack: 5,
     defense: 4
+  }.freeze
+
+  ATTACK_SKILLS_DETAIL = {
+    'slash' => { mp_cost: 5, attack_multiplier: 1.5 }
   }.freeze
 
   def initialize
@@ -96,15 +96,15 @@ class Warrior < GameCharacter
 end
 
 class Wizard < GameCharacter
-  ATTACK_SKILLS_DETAIL = {
-    'fireball' => { mp_cost: 15, attack_multiplier: 3 }
-  }.freeze
-
   LEVEL_UP_STATS_INCREMENTS = {
     hp: 15,
     mp: 20,
     attack: 3,
     defense: 2
+  }.freeze
+
+  ATTACK_SKILLS_DETAIL = {
+    'fireball' => { mp_cost: 15, attack_multiplier: 3 }
   }.freeze
 
   def initialize
@@ -118,15 +118,15 @@ class Wizard < GameCharacter
 end
 
 class Archer < GameCharacter
-  ATTACK_SKILLS_DETAIL = {
-    'multishot' => { mp_cost: 10, attack_multiplier: 2 }
-  }.freeze
-
   LEVEL_UP_STATS_INCREMENTS = {
     hp: 20,
     mp: 10,
     attack: 4,
     defense: 3
+  }.freeze
+
+  ATTACK_SKILLS_DETAIL = {
+    'multishot' => { mp_cost: 10, attack_multiplier: 2 }
   }.freeze
 
   def initialize
