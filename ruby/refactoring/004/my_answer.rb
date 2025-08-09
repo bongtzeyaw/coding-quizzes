af5bca35bdc1d40442128cfb06f671ed6abba049
bongtzeyaw <bongtzeyaw@gmail.com>
2025-08-09 13:43:41 +0900

    カスタムエラークラスを作成し、エラーハンドリングを実装

--- ruby/refactoring/004/my_answer.rb
+++ ruby/refactoring/004/my_answer.rb
@@ -1,64 +1,90 @@
 require 'net/http'
 require 'json'
 
+class WeatherAPIError < StandardError
+  private_class_method :new
+
+  attr_reader :message, :code
+
+  def initialize(message, code = nil)
+    @message = message
+    @code = code
+  end
+
+  CITY_NOT_FOUND = new('City not found', '404')
+  SERVER_ERROR   = new('Server error', '500')
+  UNKNOWN_ERROR  = new('Unknown error')
+end
+
 class WeatherAPI
   def get_weather(city)
-    return nil if [nil, ''].include?(city)
+    with_error_handling do
+      return nil if [nil, ''].include?(city)
 
-    uri = URI("https://api.weather.example.com/v1/current?city=#{city}")
-    response = Net::HTTP.get_response(uri)
+      uri = URI("https://api.weather.example.com/v1/current?city=#{city}")
+      response = Net::HTTP.get_response(uri)
 
-    if response.code == '200'
-      data = JSON.parse(response.body)
-      temp = data['temperature']
-      desc = data['description']
-      hum = data['humidity']
-      wind = data['wind_speed']
+      case response.code
+      when '200'
+        data = JSON.parse(response.body)
+        temp = data['temperature']
+        desc = data['description']
+        hum = data['humidity']
+        wind = data['wind_speed']
 
-      result = "Weather in #{city}: "
-      result += "Temperature: #{temp}\u00b0C, "
-      result += "Description: #{desc}, "
-      result += "Humidity: #{hum}%, "
-      result + "Wind: #{wind}km/h"
-
-    elsif response.code == '404'
-      'City not found'
-    elsif response.code == '500'
-      'Server error'
-    else
-      'Unknown error'
+        result = "Weather in #{city}: "
+        result += "Temperature: #{temp}\u00b0C, "
+        result += "Description: #{desc}, "
+        result += "Humidity: #{hum}%, "
+        result + "Wind: #{wind}km/h"
+      when '404'
+        raise WeatherAPIError::CITY_NOT_FOUND
+      when '500'
+        raise WeatherAPIError::SERVER_ERROR
+      else
+        raise WeatherAPIError::UNKNOWN_ERROR
+      end
     end
-  rescue StandardError => e
-    "Error: #{e.message}"
   end
 
   def get_forecast(city, days)
-    return nil if city.nil? || city == '' || days.nil? || days < 1 || days > 7
+    with_error_handling do
+      return nil if city.nil? || city == '' || days.nil? || days < 1 || days > 7
 
-    uri = URI("https://api.weather.example.com/v1/forecast?city=#{city}&days=#{days}")
-    response = Net::HTTP.get_response(uri)
+      uri = URI("https://api.weather.example.com/v1/forecast?city=#{city}&days=#{days}")
+      response = Net::HTTP.get_response(uri)
 
-    if response.code == '200'
-      data = JSON.parse(response.body)
-      forecasts = data['forecasts']
+      case response.code
+      when '200'
+        data = JSON.parse(response.body)
+        forecasts = data['forecasts']
 
-      result = "#{days}-day forecast for #{city}:\n"
-      for i in 0..forecasts.length - 1
-        date = forecasts[i]['date']
-        temp = forecasts[i]['temperature']
-        desc = forecasts[i]['description']
+        result = "#{days}-day forecast for #{city}:\n"
+        for i in 0..forecasts.length - 1
+          date = forecasts[i]['date']
+          temp = forecasts[i]['temperature']
+          desc = forecasts[i]['description']
 
-        result += "#{date}: #{temp}\u00b0C, #{desc}\n"
+          result += "#{date}: #{temp}\u00b0C, #{desc}\n"
+        end
+
+        result
+      when '404'
+        raise WeatherAPIError::CITY_NOT_FOUND
+      when '500'
+        raise WeatherAPIError::SERVER_ERROR
+      else
+        raise WeatherAPIError::UNKNOWN_ERROR
       end
-
-      result
-    elsif response.code == '404'
-      'City not found'
-    elsif response.code == '500'
-      'Server error'
-    else
-      'Unknown error'
     end
+  end
+
+  private
+
+  def with_error_handling
+    yield
+  rescue WeatherAPIError => e
+    e.message
   rescue StandardError => e
     "Error: #{e.message}"
   end
