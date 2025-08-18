b040a8debbd0c3d078140955ed164bcd05a2f5ef
t_bong <t_bong@ga-tech.co.jp>
2025-08-18 15:35:36 +0900

    EmailNotifier内EmailTemplateを使用し、通知以外の関心をEmailNotifierから分離する

--- ruby/refactoring/005/my_answer.rb
+++ ruby/refactoring/005/my_answer.rb
@@ -86,48 +86,27 @@ end
 
 class EmailNotifier
   def send_email(type, user)
-    if type == 'welcome'
-      subject = 'Welcome to Our Service!'
-      body = "Hello #{user[:name]},\n\nThank you for joining us!"
-      send_to = user[:email]
-      from = 'noreply@example.com'
-      template = 'welcome.html'
-    elsif type == 'password_reset'
-      subject = 'Password Reset Request'
-      body = "Hello #{user[:name]},\n\nClick here to reset your password."
-      send_to = user[:email]
-      from = 'security@example.com'
-      template = 'password_reset.html'
-    elsif type == 'order_confirmation'
-      subject = "Order Confirmation ##{user[:order_id]}"
-      body = "Hello #{user[:name]},\n\nYour order has been confirmed."
-      send_to = user[:email]
-      from = 'orders@example.com'
-      template = 'order.html'
-    elsif type == 'newsletter'
-      subject = 'Monthly Newsletter'
-      body = "Hello #{user[:name]},\n\nHere's our monthly update."
-      send_to = user[:email]
-      from = 'newsletter@example.com'
-      template = 'newsletter.html'
-    else
-      return false
+    with_error_handling do
+      email = EmailTemplate.new(type, user).generate
+      log_successful_email_delivery(email)
+      true
     end
+  end
 
-    mail = {}
-    mail[:to] = send_to
-    mail[:from] = from
-    mail[:subject] = subject
-    mail[:body] = body
-    mail[:template] = template
+  private
 
-    mail[:format] = if user[:preferences] && user[:preferences][:html_emails] == false
-                      'text'
-                    else
-                      'html'
-                    end
+  def with_error_handling
+    yield
+  rescue ArgumentError
+    log_failed_email_delivery
+    false
+  end
 
-    puts "Sending email: #{mail}"
-    true
+  def log_successful_email_delivery(email)
+    puts "Sending email: #{email}"
+  end
+
+  def log_failed_email_delivery
+    puts 'Failure sending email'
   end
 end
