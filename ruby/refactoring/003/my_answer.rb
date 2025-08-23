61068f1bbfa90c4116c847fdb5db98f60da5f66e
bongtzeyaw <bongtzeyaw@gmail.com>
2025-08-23 14:56:40 +0900

    SalesRecordとSalesRecordCollectionクラスを削除し、SalesReport内でヘルパーメソッドを作成

--- ruby/refactoring/003/my_answer.rb
+++ ruby/refactoring/003/my_answer.rb
@@ -1,102 +1,28 @@
 # frozen_string_literal: true
 
-class SalesRecord
-  VALID_STATUSES = %w[completed cancelled].freeze
-  VALID_CATEGORIES = %w[a b].freeze
-
-  attr_reader :amount, :status, :category, :is_vip
-
-  def initialize(amount:, status:, category:, is_vip:)
-    raise ArgumentError, 'Invalid amount' unless validate_amount(amount)
-    raise ArgumentError, 'Invalid status' unless validate_status(status)
-    raise ArgumentError, 'Invalid category' unless validate_category(category)
-    raise ArgumentError, 'Invalid is_vip' unless validate_is_vip(is_vip)
-
-    @amount = amount
-    @status = status
-    @category = category
-    @is_vip = is_vip
-  end
-
-  private
-
-  def validate_amount(amount)
-    amount.is_a?(Integer) && amount.positive?
-  end
-
-  def validate_status(status)
-    VALID_STATUSES.include?(status)
-  end
-
-  def validate_category(category)
-    VALID_CATEGORIES.include?(category)
-  end
-
-  def validate_is_vip(is_vip)
-    [true, false].include?(is_vip)
-  end
-end
-
-class SalesRecordCollection
-  def initialize(sales_data)
-    raise ArgumentError, 'Invalid sales data' unless validate_sales_data(sales_data)
-
-    @sales_records = sales_data.map { |sales_record_data| SalesRecord.new(**sales_record_data) }
-  end
-
-  def filter_by(attribute, value, negation: false)
-    filtered_sales_records = @sales_records.filter do |sales_record|
-      if negation
-        sales_record.public_send(attribute) != value
-      else
-        sales_record.public_send(attribute) == value
-      end
-    end
-
-    SalesRecordCollection.new(
-      filtered_sales_records.map do |sales_record|
-        {
-          amount: sales_record.amount,
-          status: sales_record.status,
-          category: sales_record.category,
-          is_vip: sales_record.is_vip
-        }
-      end
-    )
-  end
-
-  def extract_column(attribute)
-    @sales_records.map { |sales_record| sales_record.public_send(attribute) }
-  end
-
-  def group_by(attribute)
-    @sales_records.group_by { |sales_record| sales_record.public_send(attribute) }
-  end
-
-  private
-
-  def validate_sales_data(sales_data)
-    sales_data.is_a?(Array)
-  end
-end
-
 class SalesReport
   def generate_report(sales_data)
-    sales_record_collection = SalesRecordCollection.new(sales_data)
-
-    completed_sales_record_collection = sales_record_collection.filter_by(:status, 'cancelled', negation: true)
-    completed_sales_amounts = completed_sales_record_collection.extract_column(:amount)
+    completed_sales_records = filter_completed_sales_records(sales_data)
+    completed_sales_amounts = extract_amount(completed_sales_records)
 
     {
       total: calculate_total_sales(completed_sales_amounts),
       average: calculate_average_sales(completed_sales_amounts),
       max: calculate_max_sales(completed_sales_amounts),
-      by_category: calculate_total_sales_by_category(completed_sales_record_collection),
-      vip_sales: calculate_vip_total_sales(completed_sales_record_collection)
+      by_category: calculate_total_sales_by_category(completed_sales_records),
+      vip_sales: calculate_vip_total_sales(completed_sales_records)
     }
   end
 
   private
+
+  def filter_completed_sales_records(sales_records)
+    sales_records.filter { |record| record[:status] != 'cancelled' }
+  end
+
+  def extract_amount(sales_records)
+    sales_records.map { |record| record[:amount]}
+  end
 
   def calculate_total_sales(sales_amounts)
     sales_amounts.sum
@@ -110,12 +36,18 @@ class SalesReport
     sales_amounts.max
   end
 
-  def calculate_total_sales_by_category(sales_record_collection)
-    sales_record_collection.group_by(:category)
-                           .transform_values { |sales_record_list| sales_record_list.sum(&:amount) }
+  def calculate_total_sales_by_category(sales_records)
+    sales_records
+      .group_by { |record| record[:category] }
+      .transform_values do |category_sales_records|
+        category_sales_amounts = extract_amount(category_sales_records)
+        calculate_total_sales(category_sales_amounts)
+      end
   end
 
-  def calculate_vip_total_sales(sales_record_collection)
-    sales_record_collection.filter_by(:is_vip, true).extract_column(:amount).sum
+  def calculate_vip_total_sales(sales_records)
+    vip_sales_records = sales_records.filter { |record| record[:is_vip] }
+    vip_sales_amounts = extract_amount(vip_sales_records)
+    calculate_total_sales(vip_sales_amounts)
   end
 end
