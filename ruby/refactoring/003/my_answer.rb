fe879b7b6e1e31439f7bd6dc5a1fbe0d86c62b11
bongtzeyaw <bongtzeyaw@gmail.com>
2025-08-23 15:02:03 +0900

    extract_amountメソッドを削除し、amount列の取得と合計の計算ロジックをDRYにする

--- ruby/refactoring/003/my_answer.rb
+++ ruby/refactoring/003/my_answer.rb
@@ -3,12 +3,11 @@
 class SalesReport
   def generate_report(sales_data)
     completed_sales_records = filter_completed_sales_records(sales_data)
-    completed_sales_amounts = extract_amount(completed_sales_records)
 
     {
-      total: calculate_total_sales(completed_sales_amounts),
-      average: calculate_average_sales(completed_sales_amounts),
-      max: calculate_max_sales(completed_sales_amounts),
+      total: calculate_total_sales(completed_sales_records),
+      average: calculate_average_sales(completed_sales_records),
+      max: calculate_max_sales(completed_sales_records),
       by_category: calculate_total_sales_by_category(completed_sales_records),
       vip_sales: calculate_vip_total_sales(completed_sales_records)
     }
@@ -20,34 +19,26 @@ class SalesReport
     sales_records.filter { |record| record[:status] != 'cancelled' }
   end
 
-  def extract_amount(sales_records)
-    sales_records.map { |record| record[:amount]}
+  def calculate_total_sales(sales_records)
+    sales_records.map { |record| record[:amount]}.sum
   end
 
-  def calculate_total_sales(sales_amounts)
-    sales_amounts.sum
+  def calculate_average_sales(sales_records)
+    sales_records.empty? ? 0 : calculate_total_sales(sales_records) / sales_records.size
   end
 
-  def calculate_average_sales(sales_amounts)
-    sales_amounts.empty? ? 0 : sales_amounts.sum / sales_amounts.size
-  end
-
-  def calculate_max_sales(sales_amounts)
-    sales_amounts.max
+  def calculate_max_sales(sales_records)
+    sales_records.map { |record| record[:amount]}.max
   end
 
   def calculate_total_sales_by_category(sales_records)
     sales_records
       .group_by { |record| record[:category] }
-      .transform_values do |category_sales_records|
-        category_sales_amounts = extract_amount(category_sales_records)
-        calculate_total_sales(category_sales_amounts)
-      end
+      .transform_values { |category_sales_records| calculate_total_sales(category_sales_records) }
   end
 
   def calculate_vip_total_sales(sales_records)
     vip_sales_records = sales_records.filter { |record| record[:is_vip] }
-    vip_sales_amounts = extract_amount(vip_sales_records)
-    calculate_total_sales(vip_sales_amounts)
+    calculate_total_sales(vip_sales_records)
   end
 end
