99957955812ff4905f215bc653a35353beb696c6
t_bong <t_bong@ga-tech.co.jp>
2025-08-25 18:18:20 +0900

    recordという命名を見直す

--- ruby/refactoring/003/my_answer.rb
+++ ruby/refactoring/003/my_answer.rb
@@ -2,43 +2,43 @@
 
 class SalesReport
   def generate_report(sales_data)
-    non_cancelled_sales_records = filter_non_cancelled_sales_records(sales_data)
+    non_cancelled_sales = filter_non_cancelled_sales(sales_data)
 
     {
-      total: calculate_total_sales(non_cancelled_sales_records),
-      average: calculate_average_sales(non_cancelled_sales_records),
-      max: calculate_max_sales(non_cancelled_sales_records),
-      by_category: calculate_total_sales_by_category(non_cancelled_sales_records),
-      vip_sales: calculate_vip_total_sales(non_cancelled_sales_records)
+      total: calculate_total_sales(non_cancelled_sales),
+      average: calculate_average_sales(non_cancelled_sales),
+      max: calculate_max_sales(non_cancelled_sales),
+      by_category: calculate_total_sales_by_category(non_cancelled_sales),
+      vip_sales: calculate_vip_total_sales(non_cancelled_sales)
     }
   end
 
   private
 
-  def filter_non_cancelled_sales_records(sales_records)
-    sales_records.filter { |record| record[:status] != 'cancelled' }
+  def filter_non_cancelled_sales(sales)
+    sales.filter { |sale| sale[:status] != 'cancelled' }
   end
 
-  def calculate_total_sales(sales_records)
-    sales_records.map { |record| record[:amount] }.sum
+  def calculate_total_sales(sales)
+    sales.map { |sale| sale[:amount] }.sum
   end
 
-  def calculate_average_sales(sales_records)
-    sales_records.empty? ? 0 : calculate_total_sales(sales_records) / sales_records.size
+  def calculate_average_sales(sales)
+    sales.empty? ? 0 : calculate_total_sales(sales) / sales.size
   end
 
-  def calculate_max_sales(sales_records)
-    sales_records.map { |record| record[:amount] }.max
+  def calculate_max_sales(sales)
+    sales.map { |sale| sale[:amount] }.max
   end
 
-  def calculate_total_sales_by_category(sales_records)
-    sales_records
-      .group_by { |record| record[:category] }
-      .transform_values { |category_sales_records| calculate_total_sales(category_sales_records) }
+  def calculate_total_sales_by_category(sales)
+    sales
+      .group_by { |sale| sale[:category] }
+      .transform_values { |category_sales| calculate_total_sales(category_sales) }
   end
 
-  def calculate_vip_total_sales(sales_records)
-    vip_sales_records = sales_records.filter { |record| record[:is_vip] }
-    calculate_total_sales(vip_sales_records)
+  def calculate_vip_total_sales(sales)
+    vip_sales = sales.filter { |sale| sale[:is_vip] }
+    calculate_total_sales(vip_sales)
   end
 end
