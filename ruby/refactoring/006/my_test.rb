a8bea03507368c6c6b131ee076d918c7ff0eb9f7
bongtzeyaw <bongtzeyaw@gmail.com>
2025-08-30 12:09:03 +0900

    キーワード引数を使用し、それと伴いテストを更新

--- ruby/refactoring/006/my_test.rb
+++ ruby/refactoring/006/my_test.rb
@@ -10,7 +10,11 @@ class PaymentProcessorTest < Minitest::Test
     Time.stub :now, @fixed_time do
       PaymentProcessor.stub :sleep, nil do
         out, _ = capture_io do
-          result = PaymentProcessor.process_credit_card(100, '1234567812345678', '123')
+          result = PaymentProcessor.process_credit_card(
+            amount: 100,
+            card_number: '1234567812345678',
+            cvv: '123'
+          )
           assert_equal true, result[:success]
           assert_match /^TXN\d+$/, result[:transaction_id]
         end
@@ -27,7 +31,11 @@ class PaymentProcessorTest < Minitest::Test
   def test_process_credit_card_invalid_card_number
     Time.stub :now, @fixed_time do
       out, _ = capture_io do
-        result = PaymentProcessor.process_credit_card(100, '1234', '123')
+        result = PaymentProcessor.process_credit_card(
+          amount: 100,
+          card_number: '1234',
+          cvv: '123'
+        )
         assert_equal false, result[:success]
         assert_equal 'Invalid card number', result[:error]
       end
@@ -40,7 +48,11 @@ class PaymentProcessorTest < Minitest::Test
   def test_process_credit_card_invalid_cvv
     Time.stub :now, @fixed_time do
       out, _ = capture_io do
-        result = PaymentProcessor.process_credit_card(100, '1234567812345678', '12')
+        result = PaymentProcessor.process_credit_card(
+          amount: 100,
+          card_number: '1234567812345678',
+          cvv: '12'
+        )
         assert_equal false, result[:success]
         assert_equal 'Invalid CVV', result[:error]
       end
@@ -53,7 +65,11 @@ class PaymentProcessorTest < Minitest::Test
   def test_process_credit_card_invalid_amount
     Time.stub :now, @fixed_time do
       out, _ = capture_io do
-        result = PaymentProcessor.process_credit_card(0, '1234567812345678', '123')
+        result = PaymentProcessor.process_credit_card(
+          amount: 0,
+          card_number: '1234567812345678',
+          cvv: '123'
+        )
         assert_equal false, result[:success]
         assert_equal 'Invalid amount', result[:error]
       end
@@ -67,7 +83,11 @@ class PaymentProcessorTest < Minitest::Test
     Time.stub :now, @fixed_time do
       PaymentProcessor.stub :sleep, nil do
         out, _ = capture_io do
-          result = PaymentProcessor.process_bank_transfer(200, '12345678', '987654321')
+          result = PaymentProcessor.process_bank_transfer(
+            amount: 200,
+            account_number: '12345678',
+            routing_number: '987654321'
+          )
           assert_equal true, result[:success]
           assert_match /^BNK\d+$/, result[:transaction_id]
         end
@@ -84,7 +104,11 @@ class PaymentProcessorTest < Minitest::Test
   def test_process_bank_transfer_invalid_account_number
     Time.stub :now, @fixed_time do
       out, _ = capture_io do
-        result = PaymentProcessor.process_bank_transfer(200, '123', '987654321')
+        result = PaymentProcessor.process_bank_transfer(
+          amount: 200,
+          account_number: '123',
+          routing_number: '987654321'
+        )
         assert_equal false, result[:success]
         assert_equal 'Invalid account number', result[:error]
       end
@@ -97,7 +121,11 @@ class PaymentProcessorTest < Minitest::Test
   def test_process_bank_transfer_invalid_routing_number
     Time.stub :now, @fixed_time do
       out, _ = capture_io do
-        result = PaymentProcessor.process_bank_transfer(200, '12345678', '123')
+        result = PaymentProcessor.process_bank_transfer(
+          amount: 200,
+          account_number: '12345678',
+          routing_number: '123'
+        )
         assert_equal false, result[:success]
         assert_equal 'Invalid routing number', result[:error]
       end
@@ -110,7 +138,11 @@ class PaymentProcessorTest < Minitest::Test
   def test_process_bank_transfer_invalid_amount
     Time.stub :now, @fixed_time do
       out, _ = capture_io do
-        result = PaymentProcessor.process_bank_transfer(0, '12345678', '987654321')
+        result = PaymentProcessor.process_bank_transfer(
+          amount: 0,
+          account_number: '12345678',
+          routing_number: '987654321'
+        )
         assert_equal false, result[:success]
         assert_equal 'Invalid amount', result[:error]
       end
