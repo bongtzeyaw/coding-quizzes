27848332f4c48816ac10720b615123748d0413d2
t_bong <t_bong@ga-tech.co.jp>
2025-09-08 13:31:36 +0900

    環境変数と設定の読み込みを管理するクラスを作成

--- ruby/refactoring/015/my_answer.rb
+++ ruby/refactoring/015/my_answer.rb
@@ -3,9 +3,59 @@
 require 'yaml'
 require 'erb'
 
-class AppConfig
+class Environment
+  VALID_RAILS_ENVS = %w[production staging test development].freeze
+
+  class << self
+    def current_rails_env
+      env = ENV.fetch('RAILS_ENV', 'development')
+      raise 'Invalid environment' unless valid_rails_env?(env)
+
+      env
+    end
+
+    private
+
+    def valid_rails_env?(env)
+      VALID_RAILS_ENVS.include?(env)
+    end
+  end
+end
+
+class ConfigLoader
   CONFIG_PATH = 'config/app_config.yml'
 
+  def initialize(env)
+    @env = env
+  end
+
+  def config
+    deep_symbolize_keys(load_config[@env])
+  end
+
+  private
+
+  def load_config
+    erb_result = ERB.new(File.read(CONFIG_PATH)).result
+    YAML.load(erb_result)
+  rescue Errno::ENOENT => e
+    puts "Warning: Unable to load config file: #{e.message}"
+    {}
+  end
+
+  def deep_symbolize_keys(obj)
+    case obj
+    when Hash
+      obj.transform_keys(&:to_sym).transform_values { |v| deep_symbolize_keys(v) }
+    when Array
+      obj.map { |v| deep_symbolize_keys(v) }
+    else
+      obj
+    end
+  end
+end
+
+class AppConfig
   class << self
     def get_database_config
       config[:database]
@@ -22,30 +72,11 @@ class AppConfig
     private
 
     def env
-      ENV.fetch('RAILS_ENV', 'development')
-    end
-
-    def load_config
-      erb_result = ERB.new(File.read(CONFIG_PATH)).result
-      YAML.load(erb_result)
-    rescue Errno::ENOENT => e
-      puts "Warning: Unable to load config file: #{e.message}"
-      {}
-    end
-
-    def deep_symbolize_keys(obj)
-      case obj
-      when Hash
-        obj.transform_keys(&:to_sym).transform_values { |v| deep_symbolize_keys(v) }
-      when Array
-        obj.map { |v| deep_symbolize_keys(v) }
-      else
-        obj
-      end
+      Environment.current_rails_env
     end
 
     def config
-      deep_symbolize_keys(load_config[env])
+      ConfigLoader.new(env).config
     end
   end
 end
