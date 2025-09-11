f29acff548207eea84a641471f22fbb5b2e479d8
t_bong <t_bong@ga-tech.co.jp>
2025-09-11 11:52:25 +0900

    外部Eventの呼び出しを管理するクラスを作成し、コンフリクト検出と空きスロット検索を管理するクラスを作成

--- ruby/refactoring/016/my_answer.rb
+++ ruby/refactoring/016/my_answer.rb
@@ -82,6 +82,93 @@ class EventValidator
   end
 end
 
+class EventClient
+  class << self
+    def all
+      Event.all
+    end
+
+    def create(title, time_range)
+      event = Event.new
+      event.title = title
+      event.start_time = time_range.start_time
+      event.end_time = time_range.end_time
+      event.duration = time_range.duration_hours
+      event.save
+      event
+    end
+  end
+end
+
+class ConflictChecker
+  class << self
+    def find_conflict(time_range:, existing_events: EventClient.all)
+      existing_events.find { |event| event_overlaps?(event, time_range) }
+    end
+
+    private
+
+    def event_overlaps?(event, time_range)
+      event_time_range = TimeRange.new(start_time: event.start_time, end_time: event.end_time)
+      time_range.overlaps?(event_time_range)
+    end
+  end
+end
+
+class AvailableSlotFinder
+  class << self
+    def find(date:, slot_duration_hours:)
+      search_range = TimeRange.new(
+        start_time: Time.new(date.year, date.month, date.day, 9, 0),
+        end_time: Time.new(date.year, date.month, date.day, 18, 0)
+      )
+
+      events_in_search_range = find_events(search_range)
+
+      available_slots = []
+      current_start = search_range.start_time
+
+      events_in_search_range.each do |event|
+        if slot_available_before_event?(event, current_start, slot_duration_hours)
+          available_slots << create_slot(current_start, event.start_time)
+        end
+
+        current_start = event.end_time
+      end
+
+      if slot_available_before_time?(search_range.end_time, current_start, slot_duration_hours)
+        available_slots << create_slot(current_start, search_range.end_time)
+      end
+
+      available_slots
+    end
+
+    private
+
+    def find_events(time_range)
+      EventClient.all.select { |event| time_range.includes?(event.start_time) }
+                     .sort_by(&:start_time)
+    end
+
+    def slot_available_before_time?(time, current_start, slot_duration_hours)
+      desired_time_range = TimeRange.new(
+        start_time: current_start,
+        duration_hours: slot_duration_hours
+      )
+
+      desired_time_range.precedes?(time)
+    end
+
+    def slot_available_before_event?(event, current_start, slot_duration_hours)
+      slot_available_before_time?(event.start_time, current_start, slot_duration_hours)
+    end
+
+    def create_slot(start_time, end_time)
+      { start: start_time, end: end_time }
+    end
+  end
+end
+
 class EventScheduler
   def create_event(title, start_time_str, duration_hours)
     start_time = TimeDateParser.parse_time(start_time_str)
@@ -92,33 +179,10 @@ class EventScheduler
     validation_result = EventValidator.new(time_range).validate
     return validation_result unless validation_result[:success]
 
-    year = time_range.start_time.year
-    month = time_range.start_time.month
-    day = time_range.start_time.day
-    hour = time_range.start_time.hour
-    minute = time_range.start_time.min
+    conflict = ConflictChecker.find_conflict(time_range:)
+    return { error: "Time slot conflicts with existing event: #{conflict.title}" } if conflict
 
-    end_time = time_range.end_time
-
-    existing_events = Event.all
-    for i in 0..existing_events.length - 1
-      event = existing_events[i]
-      event_start = event.start_time
-      event_end = event.end_time
-
-      if (start_time >= event_start && start_time < event_end) ||
-         (end_time > event_start && end_time <= event_end) ||
-         (start_time <= event_start && end_time >= event_end)
-        return { error: "Time slot conflicts with existing event: #{event.title}" }
-      end
-    end
-
-    event = Event.new
-    event.title = title
-    event.start_time = start_time
-    event.end_time = end_time
-    event.duration = duration_hours
-    event.save
+    event = EventClient.create(title, time_range)
 
     { success: true, event: event }
   end
@@ -127,53 +191,6 @@ class EventScheduler
     date = TimeDateParser.parse_date(date_str)
     return { error: 'Invalid date format' } unless date
 
-    year = date.year
-    month = date.month
-    day = date.day
-
-    start_of_day = Time.new(year, month, day, 9, 0)
-    end_of_day = Time.new(year, month, day, 18, 0)
-
-    events_on_day = []
-    all_events = Event.all
-    for i in 0..all_events.length - 1
-      event = all_events[i]
-      events_on_day << event if event.start_time >= start_of_day && event.start_time < end_of_day
-    end
-
-    for i in 0..events_on_day.length - 1
-      for j in i + 1..events_on_day.length - 1
-        next unless events_on_day[i].start_time > events_on_day[j].start_time
-
-        temp = events_on_day[i]
-        events_on_day[i] = events_on_day[j]
-        events_on_day[j] = temp
-      end
-    end
-
-    available_slots = []
-    current_time = start_of_day
-
-    for i in 0..events_on_day.length - 1
-      event = events_on_day[i]
-
-      if current_time + (slot_duration_hours * 3600) <= event.start_time
-        available_slots << {
-          start: current_time,
-          end: event.start_time
-        }
-      end
-
-      current_time = event.end_time
-    end
-
-    if current_time + (slot_duration_hours * 3600) <= end_of_day
-      available_slots << {
-        start: current_time,
-        end: end_of_day
-      }
-    end
-
-    available_slots
+    AvailableSlotFinder.find(date:, slot_duration_hours:)
   end
 end
